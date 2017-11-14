3bb2545693595e81245158fc8b432addc0262815
zl-2813 <zl-2813@hotmail.com>
2017-11-14 13:55:36 +0500

    servers and users

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 set :user, "danbooru"
 set :rails_env, "staging"
-server "testbooru.donmai.us", :roles => %w(web app db), :primary => true, :user => "danbooru"
+server "testbooru", :roles => %w(web app db), :primary => true, :user => "danbooru"
 
 set :linked_files, fetch(:linked_files, []).push(".env.staging")
