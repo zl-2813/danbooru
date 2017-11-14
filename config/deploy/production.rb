eceb28e374e105bc52bbcc1276c6adb0dda13823
zl-2813 <zl-2813@hotmail.com>
2017-11-14 13:55:17 +0500

    servers and users

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
-set :user, "albert"
+set :user, "danbooru"
 set :rails_env, "production"
-server "sonohara.donmai.us", :roles => %w(web app db), :primary => true, :user => "albert"
-server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
+server "localhost", :roles => %w(web app db), :primary => true, :user => "danbooru"
+# server "hijiribe.donmai.us", :roles => %w(web app), :user => "albert"
 
 set :linked_files, fetch(:linked_files, []).push(".env.production")
