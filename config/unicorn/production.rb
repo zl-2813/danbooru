4309284e28fe5e1539c2b8a006126ca51fda9142
zl-2813 <zl-2813@hotmail.com>
2017-11-14 13:56:54 +0500

    worker_processes

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,14 +2,14 @@
 app_path = "/var/www/danbooru2/current"
 
 # Set unicorn options
-worker_processes 22
+worker_processes 2
 
 timeout 180
 #listen "127.0.0.1:9000", :tcp_nopush => true
 listen "/tmp/.unicorn.sock", :backlog => 512
 
 # Spawn unicorn master worker for user apps (group: apps)
-user 'albert', 'albert'
+user 'danbooru', 'danbooru'
 
 # Fill path to your app
 working_directory app_path
